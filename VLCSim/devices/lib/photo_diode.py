3c486d94459cb53f3be59a314035d39f91a3b0d6
bruce-morrow <bruce.morrow@hotmail.com>
2021-04-17 13:45:41 -0300

    Adding the external quantum efficiency, gain and excess noise properties to the photodiode. Adding the photocurrent and photon fluctuation noise to the photodiode. Updating the phtodiode test.

--- VLCSim/devices/lib/photo_diode.py
+++ VLCSim/devices/lib/photo_diode.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.constants as const
 from typing import Union
 
 
@@ -12,36 +13,68 @@ class PhotoDiode:
         The surface area, measured in m², which receives the light. Must be non negative
     field_of_view : float
         The field of view (FOV), i.e. the maximum incidence angle measured in radians. Must be between 0 and pi/2
+    external_quantum_efficiency : float
+        The external quantum efficiency, i.e. the number of excitons generated over the number of received photons.
+        Must be between 0 and 1, inclusive
     transmittance : float
         The light transmittance value on the surface between 0 and 1 (dimensionless). Must be between 0 and 1
+    gain : float
+        The internal gain. For PIN photodiodes it is equal to 1 (default value), for avalanche photodiodes it must be
+        greater than 1.
     optical_concentrator_refractive_index : float
         The refractive index value of the optical concentrator if is is present. Must be greater or equal to 1 (default
         None)
+    excess_noise : float
+        Excess noise factor on avalanche photodiodes due to the multiplication process, must be at least 1. On PIN
+        photodiodes it is equal to 1 (default value)
+
 
     Methods
     -------
-    get_optical_gain(psi: nd.array)
-        Returns the optical concentrator gain (dimensionless)
+    get_optical_gain(psi: Union[float, np.array])
+        Returns the optical concentrator gain (dimensionless) as a function of incidence angle
+    get_responsivity(self, wavelength: Union[float, np.array])
+        Calculates the responsivity in Ampere/Watt as a function of received wavelength
+    get_photocurrent(self, wavelength: Union[float, np.array], power: Union[float, np.array], is_outer: bool = False)
+        Calculates the generated photocurrent, in Amperes, as a function of wavelenth and received power
+    get_photon_fluctuation_noise_variance(...)
+        Calculates the variance(s) of the photon fluctuation noise
+    generate_photon_fluctuation_noise_value(...)
+        Draws value(s) from a Poisson random variable with lambda (mean and variance) equal to the photon fluctuation
+        noise variance.
     """
 
     def __init__(self,
                  area: float,
                  field_of_view: float,
+                 external_quantum_efficiency: float = 1.0,
                  transmittance: float = 1.0,
-                 optical_concentrator_refractive_index: float = None
+                 gain: float = 1.0,
+                 optical_concentrator_refractive_index: float = None,
+                 excess_noise: float = 1.0
                  ):
+        # TODO: add docstring
         if area < 0.0:
             raise ValueError("The photo diode area must be non negative.")
-        if field_of_view < 0 or field_of_view > np.pi / 2:
+        if field_of_view < 0 or field_of_view > np.pi/2:
             raise ValueError("The photo diode field of view must be between 0 and pi/2 radians.")
+        if external_quantum_efficiency < 0 or external_quantum_efficiency > 1:
+            raise ValueError("The photo diode external quantum efficiency must be between 0 and 1.")
         if transmittance < 0.0 or transmittance > 1.0:
             raise ValueError("The photo diode transmittance must be between 0 and 1.")
+        if gain < 1.0:
+            raise ValueError("The photo diode gain must at least 1")
         if optical_concentrator_refractive_index is not None and optical_concentrator_refractive_index < 1.0:
             raise ValueError("The photo diode optical concentrator refractive index must be greater or equal to 1.")
+        if excess_noise < 1.0:
+            raise ValueError("The excess noise must be at least 1.")
         self._area = area
         self._field_of_view = field_of_view
+        self._external_quantum_efficiency = external_quantum_efficiency
         self._transmittance = transmittance
+        self._gain = gain
         self._optical_concentrator_refractive_index = optical_concentrator_refractive_index
+        self._excess_noise = excess_noise
 
     @property
     def area(self) -> float:
@@ -52,8 +85,16 @@ class PhotoDiode:
         return self._field_of_view
 
     @property
+    def external_quantum_efficiency(self) -> float:
+        return self._external_quantum_efficiency
+
+    @property
     def transmittance(self) -> float:
         return self._transmittance
+
+    @property
+    def gain(self) -> float:
+        return self._gain
 
     @property
     def has_optical_concentrator(self) -> bool:
@@ -63,9 +104,64 @@ class PhotoDiode:
     def optical_concentrator_refractive_index(self) -> float:
         return self._optical_concentrator_refractive_index
 
+    @property
+    def excess_noise(self):
+        return self._excess_noise
+
+    def get_responsivity(self, wavelength: Union[float, np.array]) -> Union[float, np.array]:
+        """
+        Calculates the responsivity in Ampere/Watt as a function of received wavelength.
+
+        The responsivity is calculated as described on equation 2.19 of "GHASSEMLOOY, Zabih; POPOOLA, Wasiu;
+        RAJBHANDARI, Sujan. Optical wireless communications: system and channel modelling with Matlab®. CRC press,
+        2019."
+
+        Parameters
+        ----------
+        wavelength : Union[float, np.array]
+            The value(s) of received wavelength(s) in meters
+
+        Returns
+        -------
+        Union[float, np.array]
+            The respective responsivity(ies) in Ampere/Watt
+        """
+
+        return self.gain * wavelength * const.e * self.external_quantum_efficiency / (const.h * const.c)
+
+    def get_photocurrent(self, wavelength: Union[float, np.array], power: Union[float, np.array],
+                         is_outer: bool = False) -> Union[float, np.array]:
+        """
+        Calculates the generated photocurrent, in Amperes, as a function of wavelenth and received power.
+
+        The photocurrent can be calculated as an elementwise product, i.e. each wavelength corresponds to a power, or
+        as an outer product, i.e. for each (wavelength, power) permutation. The calculation is performed as described by
+        equation 2.18 of "GHASSEMLOOY, Zabih; POPOOLA, Wasiu; RAJBHANDARI, Sujan. Optical wireless communications:
+        system and channel modelling with Matlab®. CRC press, 2019."
+
+        Parameters
+        ----------
+        wavelength : Union[float, np.array]
+            The value(s) of received wavelength(s) in meters
+        power : Union[float, np.array]
+            The value(s) of received power in Watts
+        is_outer : bool
+            Indicates if the calculation must be carried as an outer product, otherwise it will be elementwise
+
+        Returns
+        -------
+        Union[float, np.array]
+            The respective photocurrent(s) in Amperes
+        """
+
+        if is_outer:
+            return np.outer(self.get_responsivity(wavelength), power)
+        else:
+            return self.get_responsivity(wavelength) * power
+
     def get_optical_gain(self, psi: Union[float, np.array]) -> Union[float, np.array]:
         """
-        Calculates the optical concentrator gain (dimensionless).
+        Calculates the optical concentrator gain (dimensionless) as a function of incidence angle.
 
         The gain is calculated as described on equation 3.13 of "GHASSEMLOOY, Zabih; POPOOLA, Wasiu; RAJBHANDARI, Sujan.
         Optical wireless communications: system and channel modelling with Matlab®. CRC press, 2019."
@@ -87,3 +183,65 @@ class PhotoDiode:
             return 1.0
         else:
             return np.ones(shape=psi.shape)
+
+    def get_photon_fluctuation_noise_variance(
+            self,
+            wavelength: Union[float, np.array],
+            average_power: Union[float, np.array],
+            bandwidth: Union[float, np.array]
+    ) -> Union[float, np.array]:
+        """
+        Calculates the variance(s) of the photon fluctuation noise.
+
+        The value is calculated according to equation 2.35 of "GHASSEMLOOY, Zabih; POPOOLA, Wasiu; RAJBHANDARI, Sujan.
+        Optical wireless communications: system and channel modelling with Matlab®. CRC press, 2019.", except for the
+        fact that the gain is being squared, because the generated photocurrent is already multiplied by it once
+
+        Parameters
+        ----------
+        wavelength : Union[float, np.array]
+            The value(s) of received wavelength(s) in meters
+        average_power : Union[float, np.array]
+            The value(s) of average received power in Watts
+        bandwidth : Union[float, np.array]
+            The value(s) of bandwidth of the electrical filter(s) that follows the photodiode(s)
+
+        Returns
+        -------
+        Union[float, np.array]
+            The variance value(s)
+        """
+
+        return 2.0 * const.e * self.get_photocurrent(wavelength, average_power) * bandwidth * self.excess_noise * \
+               self.gain
+
+    def generate_photon_fluctuation_noise_value(
+            self,
+            wavelength: Union[float, np.array],
+            average_power: Union[float, np.array],
+            bandwidth: Union[float, np.array],
+            num_values: int = 1
+    ) -> Union[float, np.array]:
+        """
+        Draws value(s) from a Poisson random variable with lambda (mean and variance) equal to the photon fluctuation
+        noise variance.
+
+        Parameters
+        ----------
+        wavelength : Union[float, np.array]
+            The value(s) of received wavelength(s) in meters
+        average_power : Union[float, np.array]
+            The value(s) of average received power in Watts
+        bandwidth : Union[float, np.array]
+            The value(s) of bandwidth of the electrical filter(s) that follows the photodiode(s)
+        num_values : int
+            Number of values to be drawn for each set of wavelength, average power and bandwidth (default 1)
+
+        Returns
+        -------
+        Union[float, np.array]
+            The drawn value(s)
+        """
+
+        lambdas = self.get_photon_fluctuation_noise_variance(wavelength, average_power, bandwidth)
+        return np.random.poisson(lam=lambdas, size=(num_values, lambdas.size)).T
--- VLCSim/devices/tests/test_photo_diode.py
+++ VLCSim/devices/tests/test_photo_diode.py
@@ -1,7 +1,7 @@
 import unittest
 import numpy as np
 
-from devices import PhotoDiode
+from VLCSim.devices import PhotoDiode
 
 
 class TestPhotoDiode(unittest.TestCase):
@@ -10,12 +10,15 @@ class TestPhotoDiode(unittest.TestCase):
         self._pd1 = PhotoDiode(
             area=7.45e-6,
             field_of_view=np.deg2rad(60.0),
-            transmittance=0.8
+            transmittance=0.8,
+            external_quantum_efficiency=0.75
         )
         self._pd2 = PhotoDiode(
             area=3e-6,
             field_of_view=np.deg2rad(30.0),
-            optical_concentrator_refractive_index=1.2
+            gain=2.0,
+            optical_concentrator_refractive_index=1.2,
+            excess_noise=1.5
         )
 
     def test_area(self):
@@ -23,6 +26,12 @@ class TestPhotoDiode(unittest.TestCase):
 
     def test_field_of_view(self):
         self.assertEqual(self._pd1.field_of_view, np.deg2rad(60.0), "The photo diode 1 field of view must be pi/3 rad.")
+
+    def test_external_quantum_efficiency(self):
+        self.assertEqual(self._pd1.external_quantum_efficiency, 0.75, "The photo diode 1 external quantum efficiency "
+                                                                      "must be 0.75.")
+        self.assertEqual(self._pd2.external_quantum_efficiency, 1.0, "The photo diode 2 external quantum efficiency "
+                                                                     "must be 1.")
 
     def test_transmittance(self):
         self.assertEqual(self._pd1.transmittance, 0.8, "The photo diode 1 transmittance must be 0.8.")
@@ -41,6 +50,32 @@ class TestPhotoDiode(unittest.TestCase):
         self.assertEqual(self._pd2.optical_concentrator_refractive_index, 1.2, "The photo diode 2 optical concentrator "
                                                                                "refractive index must be 1.2.")
 
+    def test_excess_noise(self):
+        self.assertEqual(self._pd1.excess_noise, 1.0, "The photo diode 1 excess noise must be 1.")
+        self.assertEqual(self._pd2.excess_noise, 1.5, "The photo diode 2 excess noise must be 1.5.")
+
+    def test_get_responsivity(self):
+        self.assertAlmostEqual(self._pd1.get_responsivity(680e-9), 0.41134274080480976,
+                               msg="The photo diode 2 responsivity must be approximately 0.41134274080480976.")
+        np.testing.assert_array_almost_equal(self._pd2.get_responsivity(np.array([500e-9, 600e-9])),
+                                             np.array([0.80655439, 0.96786527]),
+                                             err_msg="The output of an array as input must also be an array.")
+
+    def test_get_photocurrent(self):
+        self.assertAlmostEqual(self._pd1.get_photocurrent(wavelength=680e-9, power=15e-3), 0.006170141112072146)
+        np.testing.assert_array_almost_equal(self._pd2.get_photocurrent(wavelength=np.array([500e-9, 600e-9]),
+                                                                        power=np.array([15e-3, 25e-3])),
+                                             np.array([0.01209832, 0.02419663]),
+                                             err_msg="The output of arrays as input must also be an array when the "
+                                                     "product option is elementwise.")
+        np.testing.assert_array_almost_equal(self._pd2.get_photocurrent(wavelength=np.array([500e-9, 600e-9]),
+                                                                        power=np.array([15e-3, 25e-3]),
+                                                                        is_outer=True),
+                                             np.array([[0.01209832, 0.02016386],
+                                                       [0.01451798, 0.02419664]]),
+                                             err_msg="The output of arrays as input must also be a matrix when the "
+                                                     "product option is outer.")
+
     def test_get_optical_gain(self):
         self.assertEqual(self._pd1.get_optical_gain(np.deg2rad(30.0)), 1.0, "The photo diode 1 optical gain must be 1.")
         np.testing.assert_array_equal(
@@ -58,6 +93,30 @@ class TestPhotoDiode(unittest.TestCase):
                     "the FOV must be 5.76 and outside of it must be 0."
         )
 
+    def test_get_photon_fluctuation_noise_variance(self):
+        self.assertAlmostEqual(self._pd1.get_photon_fluctuation_noise_variance(680e-9, 15e-3, 1e6),
+                               1.99713116637e-15,
+                               places=16,
+                               msg="The photo diode 1 photon fluctuation noise variance must be approximately "
+                                   "1.99713116637e-15.")
+        np.testing.assert_array_almost_equal(
+            self._pd2.get_photon_fluctuation_noise_variance(wavelength=np.array([500e-9, 600e-9]),
+                                                            average_power=np.array([15e-3, 25e-3]),
+                                                            bandwidth=np.array([1e6, 2e6])),
+            np.array([1.16301874e-14, 4.65207302e-14]),
+            decimal=16,
+            err_msg="The output for arrays as input must also be an array.")
+
+    def test_generate_photon_fluctuation_noise_value(self):
+        self._pd1.get_photon_fluctuation_noise_variance(680e-9, 15e-3, 1e6)
+        self.assertEqual(
+            self._pd2.generate_photon_fluctuation_noise_value(wavelength=np.array([500e-9, 600e-9]),
+                                                              average_power=np.array([15e-3, 25e-3]),
+                                                              bandwidth=np.array([1e6, 2e6]),
+                                                              num_values=10).shape,
+            (2, 10),
+            "The generated values array shape must be (2, 10).")
+
     def test_value_error(self):
         with self.assertRaises(ValueError):
             PhotoDiode(-7.45e-6, np.pi/4)
@@ -66,11 +125,19 @@ class TestPhotoDiode(unittest.TestCase):
         with self.assertRaises(ValueError):
             PhotoDiode(7.45e-6, np.pi)
         with self.assertRaises(ValueError):
+            PhotoDiode(7.45e-6, np.pi/4, external_quantum_efficiency=-0.75)
+        with self.assertRaises(ValueError):
+            PhotoDiode(7.45e-6, np.pi/4, external_quantum_efficiency=2)
+        with self.assertRaises(ValueError):
             PhotoDiode(7.45e-6, np.pi/4, transmittance=-0.5)
         with self.assertRaises(ValueError):
             PhotoDiode(7.45e-6, np.pi / 4, transmittance=2)
         with self.assertRaises(ValueError):
             PhotoDiode(7.45e-6, np.pi / 4, optical_concentrator_refractive_index=0.5)
+        with self.assertRaises(ValueError):
+            PhotoDiode(7.45e-6, np.pi / 4, excess_noise=0.5)
+        with self.assertRaises(ValueError):
+            PhotoDiode(7.45e-6, np.pi / 4, gain=0.5)
 
 
 if __name__ == '__main__':
