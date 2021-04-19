Fixing the transmitters' angles representation and calculation.

VLCSim/systems/lib/receiver_on_plane.py
@@ -44,9 +44,10 @@ class ReceiverOnPlaneSystem(AbstractSystem):
         rx
             The receiver model object
         txs : dict
-            Dictionary where the keys are transmitter objects and the values are lists of tuples, where each tuple has
-            four elements. The first three are the x, y, and z coordinates in meters and the forth is the angle of
-            maximum emission angle in radians in relation ot the normal line
+            Dictionary where the keys are transmitter objects and the values are lists of tuples. Each tuple represents
+            the positioning of a transmitter of that type and is composed by two other tuples with three float values
+            each. The first internal tuple holds the x, y, and z coordinates in meters and the other the coordinates in
+            meters of a vector that points to maximum emission direction
         equivalent_load_resistance : float
             The equivalent input resistance, in Ohms, provided bu the electric circuit connected to the receiver, i.e.
             the resistance that the receiver sees between its outputs. Must be strictly positive
@@ -95,24 +96,23 @@ class ReceiverOnPlaneSystem(AbstractSystem):
         """
 
         rx_power = np.zeros(shape=self.plane_points.shape[:-1])
-        for tx, coords in self.transmitters.items():
-            for coord in coords:
-                point_arr = np.array(coord[:-1])
-                point_angle = coord[-1]
-                dists, phi, psi = self._get_point2plane_info(point_arr, point_angle)
+        for tx, positions in self.transmitters.items():
+            for (point_coords, point_direc) in positions:
+                dists, phi, psi = self._get_point2plane_info(point_coords, point_direc)
                 # add up the received power matrix emitted from each transmitter
                 rx_power += self.channel.calculate_rx_power(tx, self.receiver, phi, psi, dists)
         return rx_power
 
-    def _get_point2plane_info(self, point_arr: np.array, point_angle: float) -> Tuple[np.array, np.array, np.array]:
+    def _get_point2plane_info(self, point_coords: np.array, point_direc: np.array) -> \
+            Tuple[np.array, np.array, np.array]:
         """
         Calculates the distance, transmission and incidence angles from a point to every point on the plane.
 
         Parameters
         ----------
-        point_arr : np.array
+        point_coords : np.array
             An array with the point's x, y and z coordinates in meters
-        point_angle : float
+        point_direc : np.array
             The point's maximum power transmission angle, in radians, in relation to the plane's normal vector
 
         Returns
@@ -121,19 +121,10 @@ class ReceiverOnPlaneSystem(AbstractSystem):
             The distances, emission and incidence angles matrices, respectively
         """
 
-        dist_vecs = point_arr - self.plane_points
-        norm_vec = point_arr - np.array([point_arr[0], point_arr[1], 0])
+        dist_vecs = self.plane_points - point_coords
+        norm_vec = np.array([point_coords[0], point_coords[1], 0]) - point_coords
         norm_vec /= np.linalg.norm(norm_vec)
         distances = np.linalg.norm(dist_vecs, axis=2)
         psi = np.arccos(np.dot(dist_vecs, norm_vec) / distances)
-        phi: np.array
-        if point_angle != 0.0:
-            # FIXME: result is the same of when point_angle is zero
-            cos_ang = np.cos(point_angle)
-            sin_ang = np.sin(point_angle)
-            rotation_matrix = np.array([[cos_ang, -sin_ang, 0.0], [sin_ang, cos_ang, 0.0], [0.0, 0.0, 1.0]])
-            rot_dist_vecs = dist_vecs @ rotation_matrix.T
-            phi = np.arccos(np.dot(rot_dist_vecs, norm_vec) / distances)
-        else:
-            phi = psi.copy()
+        phi = np.arccos(np.dot(dist_vecs, point_direc) / (distances * np.linalg.norm(point_direc)))
         return distances, phi, psi

VLCSim/systems/tests/test_receiver_on_plane.py
@@ -25,7 +25,7 @@ class TestReceiverOnPlaneSystem(unittest.TestCase):
             num_points_axis=(3, 3),
             ch=ch,
             rx=pd,
-            txs={led: [(0.0, 0.0, 5.0, 0.0)]},
+            txs={led: [((0.0, 0.0, 5.0), (0.0, 0.0, -1.0))]},
             equivalent_load_resistance=65.4e3,
             bandwidth=4.5e6
         )
@@ -70,26 +70,36 @@ class TestReceiverOnPlaneSystem(unittest.TestCase):
                          "The output for an array as input must also be an array.")
 
     def test__get_point2plane_info(self):
-        point = np.array([0.0, 0.0, 5])
-        point_angle = 0.0
+
+        # test with pointing down
+        point_coords = np.array([0.0, 0.0, 5.0])
+        point_direc = np.array([0.0, 0.0, -1.0])
         expected_distances = np.array([[8.660254037844386, 7.071067811865476, 8.660254037844386],
                                        [7.071067811865477, 5.000000000000000, 7.071067811865476],
                                        [8.660254037844386, 7.071067811865476, 8.660254037844386]])
         expected_ang = np.array([[0.9553166181245093, np.pi/4, 0.9553166181245093],
                                  [np.pi/4, 0.00000, np.pi/4],
                                  [0.9553166181245093, np.pi/4, 0.9553166181245093]])
-        distances, phi, psi = self._sys._get_point2plane_info(point_arr=point, point_angle=point_angle)
+        distances, phi, psi = self._sys._get_point2plane_info(point_coords, point_direc)
         np.testing.assert_array_almost_equal(distances, expected_distances)
         np.testing.assert_array_almost_equal(phi, expected_ang)
         np.testing.assert_array_almost_equal(psi, expected_ang)
-        # TODO: test when point angle is different than 0
+
+        # test with pointing 30° along the diagonal of the fourth quadrant
+        point_direc = np.array([(2**0.5)/2, -(2**0.5)/2, -3**0.5])
+        expected_phi = np.array([[np.pi/3, 1.199984, 1.478915],
+                                 [0.530859, np.pi/6, 1.199984],
+                                 [0.431718, 0.530859, np.pi/3]])
+        distances, phi, psi = self._sys._get_point2plane_info(point_coords, point_direc)
+        np.testing.assert_array_almost_equal(distances, expected_distances)
+        np.testing.assert_array_almost_equal(phi, expected_phi)
+        np.testing.assert_array_almost_equal(psi, expected_ang)
 
     def test_calculate_received_power(self):
         expected_powers = np.array([[5.93291150e-07, 1.63491816e-06, 5.93291150e-07],
                                     [1.63491816e-06, 9.24849374e-06, 1.63491816e-06],
                                     [5.93291150e-07, 1.63491816e-06, 5.93291150e-07]])
         np.testing.assert_array_almost_equal(self._sys.calculate_received_power(), expected_powers)
-        # TODO: test when point angle is different than 0
 
     def test_get_snr(self):
         values = self._sys.get_snr(temperature=300.0)